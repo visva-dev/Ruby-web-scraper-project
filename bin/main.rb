b3c1fac45de99ff8b22fda10bd4cbadc6915de25
Visvaldas <visva.rapalis@gmail.com>
2020-08-17 15:52:36 +0300

    Add scrapping companies categories

--- bin/main.rb
+++ bin/main.rb
@@ -7,16 +7,6 @@ require 'open-uri'
 doc = Nokogiri::HTML(URI.open('https://www.info.lt/veiklos/pagal_abecele'))
 
 puts "### Search for nodes by css"
-doc.css('nav ul.menu li a', 'article h2').each do |link|
+doc.css('//ul.list-unstyled//li//a').each do |link|
   puts link.content
 end
-
-# puts "### Search for nodes by xpath"
-# doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
-#   puts link.content
-# end
-
-# puts "### Or mix and match."
-# doc.search('nav ul.menu li a', '//article//h2').each do |link|
-#   puts link.content
-# end
