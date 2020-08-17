44c6fa0b100380a0e959d7629dd757b775a44c2a
Visvaldas <visva.rapalis@gmail.com>
2020-08-17 18:24:52 +0300

    Add categories names and links to hash

--- bin/main.rb
+++ bin/main.rb
@@ -8,6 +8,8 @@ doc = Nokogiri::HTML(URI.open('https://www.info.lt/veiklos/pagal_abecele'))
 
 puts "### Search for nodes by css"
 doc.css('//ul.list-unstyled//li//a').each do |link|
-  puts link.content
-  puts link['href']
+  categories = { :category => link.content, :link => link['href'] }
+  # puts categories[:link]
+  # puts link.content
+  # puts link['href']
 end
