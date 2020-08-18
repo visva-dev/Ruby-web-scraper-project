fc06cd1ac3ff04845f60b8465f9cdd2f75c3837a
Visvaldas <visva.rapalis@gmail.com>
2020-08-18 10:38:12 +0300

    Add scraped data writting to a file feature

--- bin/main.rb
+++ bin/main.rb
@@ -2,14 +2,23 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
 # Fetch and parse HTML document
 doc = Nokogiri::HTML(URI.open('https://www.info.lt/veiklos/pagal_abecele'))
-
-puts "### Search for nodes by css"
 doc.css('//ul.list-unstyled//li//a').each do |link|
-  categories = { :category => link.content, :link => link['href'] }
-  # puts categories[:link]
+  categories = [link.content]
+  categories << link['href']
+  # p categories
+  # json = JSON.pretty_generate(categories)
+  File.open("categories_data.json", 'a') do |something|
+    something.puts categories.to_json
+  end
+  # categories = { :category => link.content, :url => link['href'] }
+  # puts categories[:category]
   # puts link.content
   # puts link['href']
 end
+
+# company_info = Nokogiri::HTML(URI.open('https://www.info.lt/veiklos/pagal_abecele'))
+# company_info.css('//ul.list-unstyled//li//a').each do |link|
