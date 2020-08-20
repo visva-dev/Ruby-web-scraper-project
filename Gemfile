8367dc4aee89a6df236065c87f1a00864d21ab62
Visvaldas <visva.rapalis@gmail.com>
2020-08-20 09:20:50 +0300

    Fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
+source 'https://rubygems.org'
 
-source "https://rubygems.org"
-
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "nokogiri", "~> 1.10"
-gem "httparty"
+gem 'httparty'
+gem 'nokogiri', '~> 1.10'
--- bin/main.rb
+++ bin/main.rb
@@ -7,35 +7,36 @@ class Launcher
   def initialize
     @run_program = true
   end
+
   def conect_to_page
     doc = Nokogiri::HTML(URI.open('https://www.info.lt/rubrika/Advokatai/100209472'))
   end
-  
+
   def lawyer_name
-    conect_to_page.css('//div.row//div.info//div.pavadinimas_pirmumas//a', '//div.row//div.info//div.pavadinimas//a').each do |link|
+    conect_to_page.css('//div.row//div.info//div.pavadinimas_pirmumas//a',
+                       '//div.row//div.info//div.pavadinimas//a').each do |link|
       puts lawyer = link.content
-  
     end
   end
-  
+
   def working_time
     conect_to_page.css('//div.kontaktai//div.work-time-now').each do |link|
       puts working_time = link.content
     end
   end
-  
+
   def phone_number
     conect_to_page.css('//div.kontaktai//div.telefonas').each do |link|
       puts mobile_phone = link.content
     end
   end
-  
+
   def website_link
     conect_to_page.css('//div.kontaktai//div.tinklalapis//a').each do |link|
       puts website = link['href']
     end
   end
-  
+
   def lawyer_address
     conect_to_page.css('//div.kontaktai//span.adresas-nuoroda').each do |link|
       puts address = link.content
@@ -54,20 +55,20 @@ class Launcher
   end
 
   def show_info
-    puts "Which lawyers info you want to get?"
-    puts "Write down one of these words: names, working time, numbers, websites, addressess or all to get all lawyers info's"
+    puts 'Which lawyers info you want to get? Write down one of these words:'
+    puts 'names, working time, numbers, websites, addressess or all to get all lawyers info'
     choose = gets.chomp
-    if choose == "names"
+    if choose == 'names'
       lawyer_name
-    elsif choose == "working time"
+    elsif choose == 'working time'
       working_time
-    elsif choose == "numbers"
+    elsif choose == 'numbers'
       phone_number
-    elsif choose == "websites"
+    elsif choose == 'websites'
       website_link
-    elsif choose == "addressess"
+    elsif choose == 'addressess'
       lawyer_address
-    elsif choose == "all"
+    elsif choose == 'all'
       lawyer_name
       working_time
       phone_number
