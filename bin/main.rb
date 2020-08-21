4492f35c4eb41083654b4d22154d8b487b6d18a0
Visvaldas <visva.rapalis@gmail.com>
2020-08-21 11:48:25 +0300

    Moved show_info method to get_info.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,42 +1,14 @@
 #! /usr/bin/env ruby
 require_relative '../lib/get_info.rb'
 
-class Launcher
-  def initialize
-    @run_program = true
-  end
-
-  def hello
-    puts "Hello, this is Lawyers data scraper, whould you like to get lawyers info? Type 'yes' or 'no' "
-    @run_program = gets.chomp
-    if @run_program == 'yes'
-      @run_program = true
-      show_info
-    else
-      @run_program = false
-    end
-  end
-
-  def show_info
-    puts 'Which lawyers info you want to get? Write down one of these words:'
-    puts 'names, working_time, numbers, websites, addressess or all to get all lawyers info'
-    choose = gets.chomp
-    case choose
-    when 'working_time'
-      working_time
-    when 'names'
-      lawyer_name
-    when 'numbers'
-      phone_number
-    when 'websites'
-      website_link
-    when 'addressess'
-      lawyer_address
-    when 'all'
-      all_lawyers
-    end
+def hello
+  puts "Hello, this is Lawyers data scraper, whould you like to get lawyers info? Type 'yes' or 'no' "
+  run_program = gets.chomp
+  if run_program == 'yes'
+    GetInformation.show_info
+  else
+    puts 'Ok, good luck!'
   end
 end
 
-launch = Launcher.new
-launch.hello
+hello
