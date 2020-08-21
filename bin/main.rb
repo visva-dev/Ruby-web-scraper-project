819dc8511f0c608b4b45483388c8d91ffea1a8d2
Visvaldas <visva.rapalis@gmail.com>
2020-08-21 21:34:27 +0300

    Add handle of wrong inputs

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,16 @@
 #! /usr/bin/env ruby
-require_relative '../lib/get_info.rb'
+require_relative '../lib/get_info'
 
 def hello
   puts "Hello, this is Lawyers data scraper, whould you like to get lawyers info? Type 'yes' or 'no' "
   run_program = gets.chomp
   if run_program == 'yes'
     GetInformation.show_info
-  else
+  elsif run_program == 'no'
     puts 'Ok, good luck!'
+  elsif run_program != 'yes' || run_program != 'no'
+    puts 'Please enter yes or no!'
+    hello
   end
 end
 
