a1eb0a54a5511d089da3f75c20e0d6c1f4272e9b
Visvaldas <visva.rapalis@gmail.com>
2020-08-22 09:18:30 +0300

    Moved all puts to the main file

--- bin/main.rb
+++ bin/main.rb
@@ -5,12 +5,60 @@ def hello
   puts "Hello, this is Lawyers data scraper, whould you like to get lawyers info? Type 'yes' or 'no' "
   run_program = gets.chomp
   if run_program == 'yes'
-    GetInformation.show_info
+    puts 'Which lawyers info you want to get? Write down one of these words:'
+    puts 'names, working_time, numbers, websites, addressess or all to get all lawyers info'
+    show_info
   elsif run_program == 'no'
     puts 'Ok, good luck!'
   elsif run_program != 'yes' || run_program != 'no'
     puts 'Please enter yes or no!'
     hello
+  end
+end
+
+def show_info
+  choose = gets.chomp
+  case choose
+  when 'working_time'
+    puts "Work Times"
+    puts "========"
+    puts GetInformation.working_time.text
+  when 'names'
+    puts "Names"
+    puts "========"
+    puts GetInformation.lawyer_name.text
+  when 'numbers'
+    puts "Phone Numbers"
+    puts "========"
+    puts GetInformation.phone_number.text
+  when 'websites'
+    puts "Websites Links"
+    puts "========"
+    puts GetInformation.show_links
+  when 'addressess'
+    puts "Addresses"
+    puts "========"
+    puts GetInformation.lawyer_address.text
+  when 'all'
+    puts "Names"
+    puts "========"
+    puts GetInformation.lawyer_name.text
+    puts
+    puts "Work Times"
+    puts "========"
+    puts GetInformation.working_time.text
+    puts
+    puts "Phone Numbers"
+    puts "========"
+    puts GetInformation.phone_number.text
+    puts
+    puts "Websites Links"
+    puts "========"
+    puts GetInformation.show_links
+    puts
+    puts "Addresses"
+    puts "========"
+    puts GetInformation.lawyer_address.text
   end
 end
 
--- lib/get_info.rb
+++ lib/get_info.rb
@@ -9,64 +9,30 @@ class GetInformation
   def self.lawyer_name
     conect_to_page.css('//div.row//div.info//div.pavadinimas_pirmumas//a',
                        '//div.row//div.info//div.pavadinimas//a').each do |link|
-      lawyer = link.content
-      puts lawyer
-    end
+                       end
   end
 
   def self.working_time
     conect_to_page.css('//div.kontaktai//div.work-time-now').each do |link|
-      working_time = link.content
-      puts working_time
     end
   end
 
   def self.phone_number
     conect_to_page.css('//div.kontaktai//div.telefonas').each do |link|
-      mobile_phone = link.content
-      puts mobile_phone
     end
   end
 
   def self.website_link
     conect_to_page.css('//div.kontaktai//div.tinklalapis//a').each do |link|
-      website = link['href']
-      puts website
     end
   end
 
   def self.lawyer_address
     conect_to_page.css('//div.kontaktai//span.adresas-nuoroda').each do |link|
-      address = link.content
-      puts address
     end
   end
 
-  def self.all_lawyers
-    lawyer_name
-    working_time
-    phone_number
-    website_link
-    lawyer_address
-  end
-
-  def self.show_info
-    puts 'Which lawyers info you want to get? Write down one of these words:'
-    puts 'names, working_time, numbers, websites, addressess or all to get all lawyers info'
-    choose = gets.chomp
-    case choose
-    when 'working_time'
-      working_time
-    when 'names'
-      lawyer_name
-    when 'numbers'
-      phone_number
-    when 'websites'
-      website_link
-    when 'addressess'
-      lawyer_address
-    when 'all'
-      all_lawyers
-    end
+  def self.show_links
+    links = website_link.map { |link| link['href'] }
   end
 end
